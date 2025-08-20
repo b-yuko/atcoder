ed085503f4530ef72c91a27b9776718e928a13f1
b-yuko <yuko.banno.oo@gmail.com>
2025-08-20 18:34:58 +0900

    feat: kotlin - ABC083B

--- kotlin-code/src/main/kotlin/abs/ABC083B.kt
+++ kotlin-code/src/main/kotlin/abs/ABC083B.kt
@@ -1,5 +1,43 @@
 package abs
 
+//fun someSums(){
+//    val input = readln().split(" ").map { it.toInt() }
+//    val n = input[0]
+//    val a = input[1]
+//    val b = input[2]
+//
+//    val numbers = mutableListOf<Int>()
+//    for (i in 1..n) {
+//        val num = sumOfDigits(i)
+//        if(a <= num && num <= b){
+//            numbers.add(i)
+//        }
+//    }
+//    val result = numbers.sum()
+//
+//    println(result)
+//}
+
+fun sumOfDigits(number: Int): Int {
+    return number.toString()
+        .map { it.toString().toInt() }
+        .sum()
+}
+
 fun someSums(){
-println("84")
+    val input = readln().split(" ").map { it.toInt() }
+    val n = input[0]
+    val a = input[1]
+    val b = input[2]
+
+    val numbers = mutableListOf<Int>()
+    for (i in 1..n) {
+        val num = sumOfDigits(i)
+        if(num in a..b){
+            numbers.add(i)
+        }
+    }
+    val result = numbers.sum()
+
+    println(result)
 }
